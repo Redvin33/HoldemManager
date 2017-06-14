739cfb8b5a678d02f28f2bae42f294470f85bdba
yakka34 <tv.lehtinen@hotmail.com>
2017-06-14 17:59:17 +0300

    Regex ja enum

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,29 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Main {
 
+    public enum Turn{
+        HOLECARDS,FLOP,TURN,RIVER,SHOWDOWN,SUMMARY
+    }
+
     //Linkitetty jono, koska jonon kokoa ei tiedetä etukäteen.
     private static BlockingQueue<String> queue = new LinkedBlockingQueue();
+
+    //Removes whitespaces
+    public static String trim(String old){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < old.length(); i++){
+            if (!Character.isWhitespace(old.charAt(i))){
+                sb.append(old.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
 
     public static void main(String[] args) {
         LogListener listener = new LogListener(queue);
@@ -25,12 +42,38 @@ public class Main {
     }
 
     private static void handle(){
+        Pattern turnPattern = Pattern.compile("[*]{3}.(.+).[*]{3}.?(?:\\[(.*?)\\])*.?(?:\\[(.*?)\\])*");
+
         Map<String, Player> players = new HashMap<>();
         String buttonname = "";
         while (true){
             try {
                 String line = queue.take();
-
+                Matcher m =  turnPattern.matcher(line);
+                if (m.matches()){
+                    Turn turn = Turn.valueOf(trim(m.group(1)));
+                    switch (turn){
+                        case HOLECARDS:
+                            System.out.println("PREFLOP");
+                            break;
+                        case FLOP:
+                            System.out.println("FLOP: " + m.group(2));
+                            break;
+                        case TURN:
+                            System.out.println("TURN: " + m.group(2) + ", "+ m.group(3));
+                            break;
+                        case RIVER:
+                            System.out.println("RIVER: " + m.group(2) + ", "+ m.group(3));
+                            break;
+                        case SHOWDOWN:
+                            System.out.println("SHOWDOWN");
+                            break;
+                        case SUMMARY:
+                            System.out.println("SUMMARY");
+                            break;
+                    }
+                }
+                /*
                 if (line.contains("Seat") && line.contains(":") && line.contains("chips")) {
                     String[] row = line.split("\\(");
                     String name = row[0].split(":")[1];
@@ -68,7 +111,7 @@ public class Main {
 
                     }
                 }
-
+                */
 
             } catch (InterruptedException e) {
 

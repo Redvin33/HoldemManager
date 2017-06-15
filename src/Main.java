626e43cd1b5cbf36f0a42215ed045be55b212e25
yakka34 <tv.lehtinen@hotmail.com>
2017-06-15 19:17:09 +0300

    lisää regex

--- src/Main.java
+++ src/Main.java
@@ -42,6 +42,12 @@ public class Main {
     }
 
     private static void handle(){
+        //Pls tehkää joku paremmat
+        //Matches 'PokerStars Zoom Hand #171235037798:  Hold'em No Limit ($0.01/$0.02) - 2017/06/02 5:35:03 ET'
+        Pattern handPattern = Pattern.compile("(.+)#(\\d+):\\s+(['A-Za-z\\s]+)\\(([$|€|£])(\\d+\\.\\d+)\\/[$|€|£](\\d+\\.\\d+)\\) \\- (\\d+\\/\\d+\\/\\d+) (\\d+:\\d+:\\d+) (\\w+)");
+        //Matches 'Seat 1: hirsch262 ($2.10 in chips)"
+        Pattern seatPattern = Pattern.compile("Seat.(\\d+):.(.+)\\(([$|€|£])(\\S+).in.chips\\).");
+        //Matches '*** RIVER *** [Kd 7s Ac 6c] [6d]' and '*** SHOW DOWN ***'
         Pattern turnPattern = Pattern.compile("[*]{3}.(.+).[*]{3}.?(?:\\[(.*?)\\])*.?(?:\\[(.*?)\\])*");
 
         Map<String, Player> players = new HashMap<>();
@@ -49,21 +55,29 @@ public class Main {
         while (true){
             try {
                 String line = queue.take();
-                Matcher m =  turnPattern.matcher(line);
-                if (m.matches()){
-                    Turn turn = Turn.valueOf(trim(m.group(1)));
+                Matcher handMatcher = handPattern.matcher(line);
+                Matcher seatMatcher = seatPattern.matcher(line);
+                Matcher turnMatcher =  turnPattern.matcher(line);
+                if(handMatcher.matches()){
+                    System.out.println(handMatcher.group(1) + handMatcher.group(2) + " " + handMatcher.group(3) + handMatcher.group(4)+handMatcher.group(5)+"/"+handMatcher.group(6) + " " + handMatcher.group(7) + handMatcher.group(8) + handMatcher.group(9));
+                }
+                if (seatMatcher.matches()){
+                    System.out.println("Seat " + seatMatcher.group(1) + ": " +trim(seatMatcher.group(2)) + " (" + seatMatcher.group(3) + seatMatcher.group(4) + ")");
+                }
+                if (turnMatcher.matches()){
+                    Turn turn = Turn.valueOf(trim(turnMatcher.group(1)));
                     switch (turn){
                         case HOLECARDS:
                             System.out.println("PREFLOP");
                             break;
                         case FLOP:
-                            System.out.println("FLOP: " + m.group(2));
+                            System.out.println("FLOP: " + turnMatcher.group(2));
                             break;
                         case TURN:
-                            System.out.println("TURN: " + m.group(2) + ", "+ m.group(3));
+                            System.out.println("TURN: " + turnMatcher.group(2) + ", "+ turnMatcher.group(3));
                             break;
                         case RIVER:
-                            System.out.println("RIVER: " + m.group(2) + ", "+ m.group(3));
+                            System.out.println("RIVER: " + turnMatcher.group(2) + ", "+ turnMatcher.group(3));
                             break;
                         case SHOWDOWN:
                             System.out.println("SHOWDOWN");

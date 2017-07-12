82cec76303d8e22b12866cf597ff88756c7aad71
yakka34 <tv.lehtinen@hotmail.com>
2017-07-12 15:32:31 +0300

    db muokkauksia

--- documents/database.sql
+++ documents/database.sql
@@ -14,7 +14,7 @@ create table table_seat(
   table_id int,
   seat_id int,
   nro int,
-  primary key(table_id,seat_id,nro),
+  primary key(table_id,nro),
   foreign key(table_id) references tables(id),
   foreign key(seat_id) references seats(id)
 );
@@ -40,8 +40,6 @@ create table players(
   primary key(id)
 );
 
-
-
 create table hands(
   id SERIAL,
   table_id int,
@@ -63,7 +61,7 @@ create table turns(
   foreign key(hand_id) references hands(id)
 );
 
-create table hand_player_seat(
+create table hand_player(
   id SERIAL,
   seat_nro int,
   hand_id int,
@@ -71,7 +69,6 @@ create table hand_player_seat(
   cards text[],
   primary key(id),
   foreign key(hand_id) references hands(id),
-  foreign key(seat_nro) references table_seat(nro),
   foreign key(player_id) references players(id)
 );
 
@@ -86,3 +83,52 @@ create table turn_player_action(
   foreign key(action_id) references actions(id),
   foreign key(player_id) references players(id)
 );
+
+INSERT into tables(name) VALUES('McNaught');
+INSERT into tables(name) VALUES('McDolan');
+
+INSERT into seats(role) VALUES ('btn');
+INSERT into seats(role) VALUES ('sb');
+INSERT into seats(role) VALUES ('bb');
+INSERT into seats(role) VALUES ('normal');
+
+INSERT into table_seat VALUES (1,1,1);
+INSERT into table_seat VALUES (1,2,2);
+INSERT into table_seat VALUES (1,3,3);
+INSERT into table_seat VALUES (1,4,4);
+INSERT into table_seat VALUES (1,4,5);
+INSERT into table_seat VALUES (1,4,6);
+INSERT into table_seat VALUES (2,1,1);
+INSERT into table_seat VALUES (2,2,2);
+INSERT into table_seat VALUES (2,3,3);
+INSERT into table_seat VALUES (2,4,4);
+INSERT into table_seat VALUES (2,4,5);
+INSERT into table_seat VALUES (2,4,6);
+INSERT into table_seat VALUES (2,4,7);
+INSERT into table_seat VALUES (2,4,8);
+INSERT into table_seat VALUES (2,4,9);
+
+INSERT INTO gamemodes(gamemode, currency, minstake, maxstake) VALUES ('Hold''em No Limit','USD',0.01,0.02);
+
+--Could be integers instead of varchar--
+INSERT INTO actions(action) VALUES ('fold');
+INSERT INTO actions(action) VALUES ('call');
+INSERT INTO actions(action) VALUES ('raise');
+INSERT INTO actions(action) VALUES ('check');
+
+INSERT into players(name) VALUES ('yakka34');
+INSERT into players(name) VALUES ('Redvin33');
+
+--Check if java can return timestamp that will work directly with psql--
+INSERT into hands(table_id, gamemode_id, siteid, name, date) VALUES (1,1,171235453897,'PokerStars Zoom Hand','2004-10-19 10:23:54');
+
+INSERT into turns(hand_id, phase) VALUES (1,'preflop');
+INSERT into turns(hand_id, phase, communitycards) VALUES (1,'flop','{"7c","Ac","2c"}');
+INSERT into turns(hand_id, phase, communitycards) VALUES (1,'turn','{"7c","Ac","2c","Ah"}');
+
+INSERT into hand_player(seat_nro, hand_id, player_id, cards) VALUES (1,1,1,'{"Ad","Kd"}');
+INSERT into hand_player(seat_nro, hand_id, player_id, cards) VALUES (2,1,2,'{"Qd","Jd"}');
+
+INSERT INTO turn_player_action(player_id, action_id, turn_id, amount) VALUES (1,1,1,0.0);
+INSERT INTO turn_player_action(player_id, action_id, turn_id, amount) VALUES (2,2,1,5.0);
+INSERT INTO turn_player_action(player_id, action_id, turn_id, amount) VALUES (2,4,2,0.0);
